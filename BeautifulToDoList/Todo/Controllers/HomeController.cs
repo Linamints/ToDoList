d36fdb5532936f7dead0bdb87e1ebbbaa0913b4b
Linamints <97270902+Linamints@users.noreply.github.com>
2022-07-18 02:21:28 -0700

    Add files via upload

---
+++ BeautifulToDoList/Todo/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Logging;
+using Todo.Models;
+
+namespace Todo.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public RedirectResult Insert(TodoItem todo)
+    {
+        using (SqliteConnection con = 
+               new SqliteConnection("Data Source=db.sqlite"))
+        {
+            using (var tableCmd = con.CreateCommand())
+            {
+                con.Open();
+                tableCmd.CommandText = $"INSERT INTO todo (name) VALUES ('{todo.Name}')";
+                try
+                {
+                    tableCmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+          
+        }
+        ///Removed local host address for debugging purposes as I'm not sure if my local host matches others.
+        return Redirect("(Insert your local host here for testing purposes)");
+    }
+}
